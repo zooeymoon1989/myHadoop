7fcff2ff4b89c5de41f58982718e34e82edf6409
zooeymoon1989 <liwenqiang@pwrd.com>
2021-12-09 11:40:55 +0800

    add mapper and reducer

---
+++ src/main/java/WordCountMapper/wordCount/myMapper.java
@@ -0,0 +1,42 @@
+package WordCountMapper.wordCount;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.*;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.StringTokenizer;
+
+public class myMapper {
+    public static class SampleMapper extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
+        private final static IntWritable count = new IntWritable(1);
+        private final Text text = new Text();
+
+        @Override
+        public void map(LongWritable longWritable, Text value, OutputCollector<Text, IntWritable> outputCollector, Reporter reporter) throws IOException {
+            String line = value.toString();
+            StringTokenizer tokenizer = new StringTokenizer(line);
+            while (tokenizer.hasMoreTokens()) {
+                text.set(tokenizer.nextToken());
+                outputCollector.collect(text, count);
+            }
+        }
+    }
+
+    public static class SampleReducer extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
+
+        @Override
+        public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
+            int sum = 0;
+            while (values.hasNext()) {
+                sum += values.next().get();
+            }
+
+            output.collect(key,new IntWritable(sum));
+        }
+    }
+}
+
+
--- src/main/java/liwenqiang/hello.java
+++ src/main/java/liwenqiang/hello.java
@@ -1,25 +1,44 @@
 package liwenqiang;
 
+import WordCountMapper.wordCount.myMapper;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.LocatedFileStatus;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.fs.RemoteIterator;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.*;
 
 import java.io.IOException;
 import java.net.URI;
 
 public class hello {
     public static void main(String[] args) throws IOException {
-        URI uri = URI.create ("hdfs://10.3.250.19:9000");
-        Configuration conf = new Configuration();
-//        conf.set("fs.default.name","hdfs://10.3.250.19:9000");
-        FileSystem file = FileSystem.get(uri,conf);
-        RemoteIterator<LocatedFileStatus> listFiles = file.listFiles(new Path("/foo"), false);
-        while (listFiles.hasNext()) {
-            LocatedFileStatus fileStatus = listFiles.next();
-            System.out.println(fileStatus.getPath());
-        }
-        System.out.println("done");
+//        URI uri = URI.create ("hdfs://10.3.250.19:9000");
+//        Configuration conf = new Configuration();
+////        conf.set("fs.default.name","hdfs://10.3.250.19:9000");
+//        FileSystem file = FileSystem.get(uri,conf);
+//        RemoteIterator<LocatedFileStatus> listFiles = file.listFiles(new Path("/foo"), false);
+//        while (listFiles.hasNext()) {
+//            LocatedFileStatus fileStatus = listFiles.next();
+//            System.out.println(fileStatus.getPath());
+//        }
+//        System.out.println("done");
+        String inputPath=args[0];
+        String outputPath=args[1];
+        JobConf conf = new JobConf(myMapper.class);
+        conf.setJobName("SampleMapReduce");
+        conf.setOutputKeyClass(Text.class);
+        conf.setOutputValueClass(IntWritable.class);
+        conf.setMapperClass(myMapper.SampleMapper.class);
+        conf.setCombinerClass(myMapper.SampleReducer.class);
+        conf.setReducerClass(myMapper.SampleReducer.class);
+        conf.setInputFormat(TextInputFormat.class);
+        conf.setOutputFormat(TextOutputFormat.class);
+        FileInputFormat.setInputPaths(conf, new
+                Path(inputPath)); FileOutputFormat.setOutputPath(conf, new
+                Path(outputPath));
+        JobClient.runJob(conf);
     }
 }
