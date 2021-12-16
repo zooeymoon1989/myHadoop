8f26449a275b4300dab089e3f6b0dc9696314b1c
zooeymoon1989 <liwenqiang@pwrd.com>
2021-12-16 14:55:27 +0800

    add Graph

---
+++ src/main/java/util/dataStructure/Graph.java
@@ -0,0 +1,18 @@
+package util.dataStructure;
+
+import java.util.Map;
+
+public class Graph {
+
+    public boolean isDirected;
+    public int[][] vertices;
+    public Map<String, String> adjList;
+
+    public Graph(boolean isDirected) {
+        this.isDirected = isDirected;
+    }
+
+    public void addVertex(int v) {
+
+    }
+}
